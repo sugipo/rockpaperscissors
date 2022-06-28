5a82a718cf5c1e48a73f08e58af8253e428d3622
Kyle <kbkiragaya@gmail.com>
2022-06-28 02:28:22 -0700

    Managed to finish user input and rock paper scissors game, now to make it 5 rounds

--- java.js
+++ java.js
@@ -1,20 +1,36 @@
-
+var computer = " ";
 function computerSelection() {
     const rndInt = Math.floor(Math.random() * 3) + 1;
     if (rndInt == 1){
-        console.log("Rock");
+        return computer = "rock";
+        
     } 
     else if (rndInt == 2) {
-        console.log("Paper");
+        return computer = "paper";
     } 
     else {
-        console.log("Scissor");
+        return computer = "scissors";
     }
-}
 
-function playerSelection(form){
+}
+console.log(computerSelection());
+function playerSelection(form, computerSelection){
     var InputVar = form.inputbox.value.toLowerCase();
-    
-    if 
+    var computerSelection = computer;
+    if (InputVar == "rock" || InputVar == "paper" || InputVar == "scissors"){
+        console.log(InputVar);
+        console.log(computerSelection);
+        if (InputVar == "rock" && computerSelection == "rock" || InputVar == "paper" && computerSelection == "paper" || InputVar == "scissors" && computerSelection == "scissors")
+        {
+            alert("Tie no winner");
+        } else if(InputVar == "rock" && computerSelection == "paper" || InputVar == "paper" && computerSelection == "scissors" || InputVar == "scissors" && computerSelection =="rock"){
+            alert("You Lose! " + computerSelection + " bests " + InputVar);
+        }else if(InputVar == "rock" && computerSelection == "scissors" || InputVar == "paper" && computerSelection == "rock" || InputVar == "scissors" && computerSelection == "paper"){
+            alert("You Win! " + InputVar + " beats " + computerSelection)
+        }
+    } else {
+        alert("Please enter Rock, Paper, or Scissors in the text box");
+    }
+       
 }
 
