f1c996e5b984418942f671a5daf81ab10129673f
Kyle <kbkiragaya@gmail.com>
2022-06-28 01:23:34 -0700

    created computerselection function in java.js working on play selection and input

---
+++ java.js
@@ -0,0 +1,20 @@
+
+function computerSelection() {
+    const rndInt = Math.floor(Math.random() * 3) + 1;
+    if (rndInt == 1){
+        console.log("Rock");
+    } 
+    else if (rndInt == 2) {
+        console.log("Paper");
+    } 
+    else {
+        console.log("Scissor");
+    }
+}
+
+function playerSelection(form){
+    var InputVar = form.inputbox.value.toLowerCase();
+    
+    if 
+}
+
